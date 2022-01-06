2c24ffa81a2097a133196175e80a5deba6825074
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2022-01-06 14:45:52 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # copy-paste-hack-blocker
 This is a Chrome plugin that protects the browser from the copy paste hack that is shown here https://marco97pa.github.io/copy-paste-hack-js/
+
+By default a confirmation dialogue will be displayed if the copied text is different than the selected text the user can change the setting to have the copy be auto corrected without confirmation.
+
--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 chrome.storage.sync.get({
     enabled : ''
 }, function(items) {
-    console.log(items)
-    if (items.enabled) {
-        function removeCopyListeners() {
-            const allElements = Array.prototype.slice.call(document.querySelectorAll('*'));
-            allElements.push(document);
-            allElements.push(window);
-            let elements = [];
-            for (let element of allElements) {
-            console.log(element)
-                if (getEventListeners(element).copy) {
-                    for (let listener of getEventListeners(element).copy) {
-                        element.removeEventListener('copy', listener.listener)
-                    }
-                }
-            }
-            setTimeout(removeCopyListeners(), 800)
+    document.addEventListener('copy', (event) => {
+        let clip = event.clipboardData.getData('text/plain')
+        const selection = document.getSelection().toString()
+        const message = `
+The text that was copied clipboard does not match selected text.
+The selected starts with: 
+
+${selection.slice(0, 400)}
+
+The text was copied to your clipboard starts with:
+
+${clip.slice(0, 400)}
+
+Do you want to put the selected text in your clipboard instead?`
+        if (clip && selection !== clip && (items.enabled || confirm(message))) {
+            event.clipboardData.setData('text/plain', selection.toString())
         }
-        removeCopyListeners()
-    }
+    })
 })
