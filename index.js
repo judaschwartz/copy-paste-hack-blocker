4c0d83a302e0bd78ccb2f0b8f128c269113704b6
Yehuda Schwartz <yehuda.schwartz@hcl.com>
2022-01-11 11:05:07 -0500

    address stop propagation

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 chrome.storage.sync.get({
     enabled : ''
 }, function(items) {
-    document.addEventListener('copy', (event) => {
-        let clip = event.clipboardData.getData('text/plain')
-        const selection = document.getSelection().toString()
-        const message = `
-The text that was copied clipboard does not match selected text.
-The selected starts with: 
+    document.addEventListener('copy', (e) => {
+        setTimeout(checkCopy, 10, document.getSelection().toString())
+        function checkCopy (selection) {
+            const input = document.createElement("input")
+            document.body.appendChild(input)
+            input.focus()
+            document.execCommand("paste")
+            const clip = input.value
+            document.body.removeChild(input)
+            const message = `
+The text copied to your clipboard does not match the selected text.
+The text selected on this page starts with: 
 
 ${selection.slice(0, 400)}
 
-The text was copied to your clipboard starts with:
+The text copied to your clipboard starts with:
 
 ${clip.slice(0, 400)}
 
 Do you want to put the selected text in your clipboard instead?`
-        if (clip && selection !== clip && (items.enabled || confirm(message))) {
-            event.clipboardData.setData('text/plain', selection.toString())
+            if (clip && selection !== clip && (items.enabled || confirm(message))) {
+                const i = document.createElement('textarea')
+                i.innerHTML = selection
+                document.body.appendChild(i)
+                i.select()
+                document.execCommand('copy')
+                document.body.removeChild(i)
+            }
         }
-    })
+    }, true)
 })
