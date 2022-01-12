b1386d29c3ced71e841c53adf6f924d26c3ed66f
Yehuda Schwartz <judaschwartz@users.noreply.github.com>
2022-01-12 07:15:39 -0500

    address stop propagation (#2)
    * address stop propagation
    
    * fix multiline
    
    * add cut and fix nbsp char

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 chrome.storage.sync.get({
     enabled : ''
 }, function(items) {
-    document.addEventListener('copy', (event) => {
-        let clip = event.clipboardData.getData('text/plain')
-        const selection = document.getSelection().toString()
+    document.addEventListener('copy', (e) => {
+        setTimeout(checkCopy, 10, document.getSelection().toString())
+    }, true)
+    document.addEventListener('cut', (e) => {
+        setTimeout(checkCopy, 10, document.getSelection().toString())
+    }, true)
+    function checkCopy (selection) {
+        const input = document.createElement("textarea")
+        input.style.position = "fixed"
+        document.body.appendChild(input)
+        input.focus()
+        document.execCommand("paste")
+        const clip = input.value
+        document.body.removeChild(input)
         const message = `
-The text that was copied clipboard does not match selected text.
-The selected starts with: 
+The text copied to your clipboard does not match the selected text.
+The text selected on this page starts with: 
 
 ${selection.slice(0, 400)}
 
-The text was copied to your clipboard starts with:
+The text copied to your clipboard starts with:
 
 ${clip.slice(0, 400)}
 
 Do you want to put the selected text in your clipboard instead?`
-        if (clip && selection !== clip && (items.enabled || confirm(message))) {
-            event.clipboardData.setData('text/plain', selection.toString())
+        if (clip && selection !== clip) {
+            const i = document.createElement('textarea')
+            i.innerHTML = selection
+            selection = i.value.replace(/\u00a0/g, ' ')
+            if (selection !== clip && (items.enabled || confirm(message))) {
+                document.body.appendChild(i)
+                i.select()
+                document.execCommand('copy')
+                document.body.removeChild(i)
+            }
         }
-    })
+    }
 })
